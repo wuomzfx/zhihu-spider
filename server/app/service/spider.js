130a551b6fc6a1b265a71371d81e6d0891616f81
XIANG\xiang <xiang@dripower.com>
2017-05-01 11:48:58 +0800

    禁止知乎gzip

--- server/app/service/spider.js
+++ server/app/service/spider.js
@@ -1,11 +1,18 @@
 const request = require('request-promise-native')
+// request = request.defaults({'proxy': 'http://127.0.0.1:1080'})//走本地代理做测试
 const cheerio = require('cheerio')
 const config = require('../config')
 const zhihuRoot = config.zhihu.root
 
 module.exports = {
   async getData (qid) {
-    const rs = await request(`${zhihuRoot}/question/${qid}`).catch(err => {
+    const options = {
+      url: `${zhihuRoot}/question/${qid}`,
+      headers: {
+        'Accept-Encoding': 'deflate, sdch, br' // 不允许gzip,开启gzip会开启知乎客户端渲染，导致无法爬取
+      }
+    }
+    const rs = await request(options).catch(err => {
       return err
     })
     if (rs.error) {
