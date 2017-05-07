c695ae06165daba9f69a8aa8441bb580a6dfc628
XIANG\xiang <xiang@dripower.com>
2017-05-07 22:07:23 +0800

    修改任务轮询时间

--- server/app/schedule/index.js
+++ server/app/schedule/index.js
@@ -4,8 +4,8 @@ const data = require('./data')
 // 每分钟执行一次
 const rule = {
   start: new Date(Date.now()),
-  end: new Date(+new Date() + 1200),
-  rule: '*/1 * * * * *'
+  // end: new Date(+new Date() + 1200),
+  rule: '*/10 * * * * *'
 }
 schedule.scheduleJob(rule, () => {
   console.log('spider data start')
