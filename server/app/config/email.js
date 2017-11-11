49e76bdb0e75bfb1ccaa978c898ea56fea54c724
wuomzfx <75851654@qq.com>
2017-11-11 14:09:45 +0800

    add email config demo

---
+++ server/app/config/email.js
@@ -0,0 +1,11 @@
+module.exports = {
+  transporter: {
+    host: 'smtp server of email',
+    secure: true,
+    port: 465,
+    auth: {
+      user: 'email address',
+      pass: 'email password'
+    }
+  }
+}
